d2b95069f17f04e83ae5145f9d20594c43b35774
Diane <hello@dianenguyen.ca>
2023-11-30 15:26:20 -0800

    Completed exercise for glass ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const plugin = require('tailwindcss/plugin')
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -12,7 +15,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '2px 2px 2px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
